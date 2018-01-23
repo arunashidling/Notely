4a23c34b14677f2fb250a9679b87735878486ce1
Aruna <aruna.g@hubblehome.com>
2018-01-24 01:59:16 +0530

    Added notes view activity

--- Notely/app/src/main/java/com/pe/notes/ui/NotesAdapter.java
+++ Notely/app/src/main/java/com/pe/notes/ui/NotesAdapter.java
@@ -1,6 +1,7 @@
 package com.pe.notes.ui;
 
 import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -8,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -29,8 +32,9 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
 
     public class NotesViewHolder extends RecyclerView.ViewHolder {
         public TextView mHeader, mSubHeading, mDate;
-        public ImageButton mFav, mStar;
+
         public LinearLayout mcontentPanel;
+        public CheckBox mFav, mStar;
 
 
         public NotesViewHolder(View view) {
@@ -38,8 +42,8 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
             mHeader = (TextView) view.findViewById(R.id.text1);
             mSubHeading = (TextView) view.findViewById(R.id.subheading);
             mDate = (TextView) view.findViewById(R.id.date);
-            mFav = (ImageButton) view.findViewById(R.id.fav);
-            mStar = (ImageButton) view.findViewById(R.id.star);
+            mFav = (CheckBox) view.findViewById(R.id.fav);
+            mStar = (CheckBox) view.findViewById(R.id.star);
             mcontentPanel = (LinearLayout) view.findViewById(R.id.contentPanel);
 
         }
@@ -67,40 +71,88 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
         holder.mHeader.setText(note.getmHeader());
         holder.mSubHeading.setText(note.getmSubHeading());
         holder.mDate.setText(note.getmDate());
-        if(note.getmFavourite() == 1)
-             holder.mFav.setBackgroundResource(R.drawable.heart_red);
-        else
-            holder.mFav.setBackgroundResource(R.drawable.heart_gray);
 
-        if(note.getmStar() == 1)
-            holder.mStar.setBackgroundResource(R.drawable.start_yello);
-        else
-            holder.mStar.setBackgroundResource(R.drawable.start_gray);
+        if(note.getmFavourite() == 1) {
+            holder.mFav.setChecked(true);
+            holder.mFav.setButtonDrawable(R.drawable.heart_red);
+        }
+        else {
+            holder.mFav.setChecked(false);
+            holder.mFav.setButtonDrawable(R.drawable.heart_gray);
+        }
+
+        holder.mFav.setTag(note);
+
+        if(note.getmStar() == 1) {
+            holder.mStar.setChecked(true);
+            holder.mStar.setButtonDrawable(R.drawable.start_yello);
+        }
+        else {
+            holder.mStar.setChecked(false);
+            holder.mStar.setButtonDrawable(R.drawable.start_gray);
+        }
+        holder.mStar.setTag(note);
 
         holder.mcontentPanel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 long id = note.getId();
                 Uri uri = ContentUris.withAppendedId(data, id);
-                Intent intent = new Intent(Intent.ACTION_EDIT, uri);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 mContext.startActivity(intent);
             }
         });
 
-        holder.mFav.setOnClickListener(new View.OnClickListener() {
+        holder.mFav.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                holder.mFav.setBackgroundResource(R.drawable.heart_red);
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Notes note = (Notes) compoundButton.getTag();
+                if(compoundButton.isChecked()){
+                    compoundButton.setButtonDrawable(R.drawable.heart_red);
+                    note.setmFavourite(1);
+                }else{
+                    compoundButton.setButtonDrawable(R.drawable.heart_gray);
+                    note.setmFavourite(0);
+                }
+
+                updateNote(note);
             }
         });
 
-        holder.mStar.setOnClickListener(new View.OnClickListener() {
+
+        holder.mStar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                holder.mStar.setBackgroundResource(R.drawable.start_yello);
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Notes note = (Notes) compoundButton.getTag();
+                if(compoundButton.isChecked()){
+                    compoundButton.setButtonDrawable(R.drawable.start_yello);
+                    note.setmStar(1);
+                }else{
+                    compoundButton.setButtonDrawable(R.drawable.start_gray);
+                    note.setmStar(0);
+                }
+
+                updateNote(note);
             }
         });
+    }
+
+    private void updateNote(Notes note) {
+               // Sets up a map to contain values to be updated in the provider.
+            ContentValues values = new ContentValues();
+            values.put(NotePad.Notes.COLUMN_NAME_FAVOURITE, note.getmFavourite());
+            values.put(NotePad.Notes.COLUMN_NAME_STAR, note.getmStar());
+
+            mContext.getContentResolver().update(
+                    data,    // The URI for the record to update.
+                    values,  // The map of column names and new values to apply to them.
+                    NotePad.Notes.COLUMN_NAME_TITLE +" =?",    // No selection criteria are used, so no where columns are necessary.
+                    new String[] {note.getmHeader()}     // No where columns are used, so no where arguments are necessary.
+            );
+
+
+
     }
 
     @Override
--- Notely/app/src/main/java/com/pe/notes/ui/ViewNoteActivity.java
+++ Notely/app/src/main/java/com/pe/notes/ui/ViewNoteActivity.java
@@ -1,30 +1,134 @@
 package com.pe.notes.ui;
 
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 public class ViewNoteActivity extends AppCompatActivity {
+    Button edit;
+    ImageButton back;
+    TextView note,date, header;
+    private Uri mUri;
+    private Cursor mCursor;
+
+    private static final String TAG = "ViewNoteActivity";
+
+    private static final String[] PROJECTION =
+            new String[] {
+                    NotePad.Notes._ID,
+                    NotePad.Notes.COLUMN_NAME_TITLE,
+                    NotePad.Notes.COLUMN_NAME_NOTE,
+                    NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_note);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Intent intent = getIntent();
+
+        /*
+         *  Sets up for the edit, based on the action specified for the incoming Intent.
+         */
+
+        // Gets the action that triggered the intent filter for this Activity
+        final String action = intent.getAction();
+
+        // For an edit action:
+        if (Intent.ACTION_VIEW.equals(action)) {
+
+            // Sets the Activity state to EDIT, and gets the URI for the data to be edited.
+
+            mUri = intent.getData();
+
+            // For an insert or paste action:
+        } else  {
+
+            // Logs an error that the action was not understood, finishes the Activity, and
+            // returns RESULT_CANCELED to an originating Activity.
+            Log.e(TAG, "Unknown action, exiting");
+            finish();
+            return;
+        }
+
+        /*
+         * Using the URI passed in with the triggering Intent, gets the note or notes in
+         * the provider.
+         * Note: This is being done on the UI thread. It will block the thread until the query
+         * completes. In a sample app, going against a simple provider based on a local database,
+         * the block will be momentary, but in a real app you should use
+         * android.content.AsyncQueryHandler or android.os.AsyncTask.
+         */
+        mCursor = managedQuery(
+                mUri,         // The URI that gets multiple notes from the provider.
+                PROJECTION,   // A projection that returns the note ID and note content for each note.
+                null,         // No "where" clause selection criteria.
+                null,         // No "where" clause selection values.
+                null          // Use the default sort order (modification date, descending)
+        );
+
+
+
+        setContentView(R.layout.activity_view_note);
+        edit = (Button) findViewById(R.id.viewedit);
+        back = (ImageButton) findViewById(R.id.viewback);
+        note = (TextView) findViewById(R.id.noteview);
+        date = (TextView) findViewById(R.id.dateview);
+        header = (TextView) findViewById(R.id.headerview);
+
+
+        edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               // long id = note.getId();
+                //Uri uri = ContentUris.withAppendedId(data, id);
+                Intent intent = new Intent(Intent.ACTION_EDIT, mUri);
+
+                startActivity(intent);
             }
         });
+
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mCursor != null) {
+            // Requery in case something changed while paused (such as the title)
+            mCursor.requery();
+
+            /* Moves to the first record. Always call moveToFirst() before accessing data in
+             * a Cursor for the first time. The semantics of using a Cursor are that when it is
+             * created, its internal index is pointing to a "place" immediately before the first
+             * record.
+             */
+            mCursor.moveToFirst();
+            int colTitleIndex = mCursor.getColumnIndex(NotePad.Notes.COLUMN_NAME_TITLE);
+            String title = mCursor.getString(colTitleIndex);
+            header.setText(title);
+
+            int colNotesIndex = mCursor.getColumnIndex(NotePad.Notes.COLUMN_NAME_NOTE);
+            String noteText = mCursor.getString(colNotesIndex);
+            note.setText(noteText);
+
+            int colDateIndex = mCursor.getColumnIndex(NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE);
+            String notedate = mCursor.getString(colDateIndex);
+            date.setText(notedate);
+
+        }
+    }
 }
